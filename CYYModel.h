e305bb6f650acaa4ca6faf1f1c26e381d7b7b171
caoyuanyuan <1398289685@qq.com>
2019-08-06 14:10:00 +0800

    CYYKit add model

---
+++ CYYModel.h
@@ -0,0 +1,16 @@
+//
+//  CYYModel.h
+//  CYYKit
+//
+//  Created by CaoYuanyuan on 2019/8/6.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CYYModel : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CYYModel.m
@@ -0,0 +1,12 @@
+//
+//  CYYModel.m
+//  CYYKit
+//
+//  Created by CaoYuanyuan on 2019/8/6.
+//
+
+#import "CYYModel.h"
+
+@implementation CYYModel
+
+@end
